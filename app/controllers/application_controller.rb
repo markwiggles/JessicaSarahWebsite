8ce87b4800e42bb7736092a90ff3c1c2a409e71b
markwiggles <markwigg@live.com>
2014-09-02 16:19:34 +1000

    added contact model and form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,19 @@ class ApplicationController < ActionController::Base
 
   layout 'default'
 
+  before_filter 'assign_links'
+
+
+  def assign_links
+
+    @twitter = 'https://www.twitter.com/Jsarahmusic'
+    @facebook = 'https://www.facebook.com/pages/Jess-Wigglesworth/304789669623158'
+    @youtube = 'http://www.youtube.com/user/JessWigglesworth'
+    @tumblr = 'http://www.jessicasarahmusic.tumblr.com/'
+    @instagram = 'http://www.instagram.com/jessicasarahmusic/'
+
+  end
+
+
+
 end
--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -1,12 +1,30 @@
 class FollowController < ApplicationController
 
 
-  def follow
-
-  end
-
-
   def contact
 
+    @contact = Contact.new
+
   end
+
+  def create_contact
+
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = 'Message sent'
+    else
+      render('contact')
+    end
+
+  end
+
+  def contact_params
+
+    params.require(:contact).permit(
+        :message,
+        :name,
+        :email
+    )
+  end
+
 end
