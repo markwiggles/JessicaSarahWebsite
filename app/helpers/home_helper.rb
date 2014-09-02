bbec81f0c4b8dc25feb339a78da09ba7a2ebd5b6
markwiggles <markwigg@live.com>
2014-09-03 08:20:55 +1000

    some edits

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,14 +3,17 @@ module HomeHelper
 
   PAGE_ID = 'jessicasarahmusic'
 
-  ACCESS_TOKEN = "CAACEdEose0cBAGZALPtTmxtKC2ZBDpV1PlciWhG0UpXPXuk2FC0aIA3X1aclZAvb1J8HLsMghUErWk6OWIj6wAzdRqRSKbj1agSzKyHWx2FcwnduagZCAsmZAlSzYA7F2fKXHZCRpsWQpEcqsnXLvdXwAwxG3kY3Nw9bDhcPzxYG4GGgFRevrtJAZAIWl0OLiUFCgZAyCPKjsU76Uc52lLVu"
+  ACCESS_TOKEN = "CAACEdEose0cBAMMt7dDWfZCyQcoknEE5xgKNp3DyBgLmnYBIV9ZCYDuvcB8ZCiz2E6aBg44mgubXJTiXb8BtkN2A93LRQn8T2ZAQlDOHv5ZBZCZBLq9AnMwYyN0xEVNOhYZCH1KyYzEk7esp6RmiWBVPsIJ2H804ccBEWQ3JrRsly89wOzP5g7PWRWs7UQbe7YP2LWu9RetQWcfZAPnKZCciZAKuTTrGY55niMZD"
 
 
   def get_facebook_post
 
     @graph = Koala::Facebook::API.new(ACCESS_TOKEN)
 
-    profile = @graph.get_connection('jessicasarahmusic', 'feed')
+    page = @graph.get_connection('jessicasarahmusic', 'statuses')
+
+
+
 
   end
 
